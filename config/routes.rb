dd7d4067c72f7b8aac473d248c4e05c91a5d67ac
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 14:11:24 +0000

    basic button functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :portfolios, exept: [:show]
-  get 'portfolio/:id', to: 'portfolio#show', as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
 
-  resources :blogs
+  resources :blogs do 
+    member do
+      get :toggle_status
+    end
+    end
   
   root to: 'pages#home'
 end
