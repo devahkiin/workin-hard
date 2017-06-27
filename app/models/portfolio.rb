98f65f4def69ac308b5bf24cbb05dc18e07f752b
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:21:01 +0000

    implemented image defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,13 @@ class Portfolio < ApplicationRecord
   end  
   
   scope :ruby_on_rails, -> { where(subtitle: 'ruby on rails') }
+  
+  after_initalize :set_defualt
+  
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end  
 end
+
+
