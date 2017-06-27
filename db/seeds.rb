77aa1d4a42380433cd5c15be388610d8c82d1313
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 11:22:03 +0000

    implemented custom scopesfor portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Bananas are great. They are the best in the world. Oh how i do wish i was a banana. Oh banana oh banana, please take me away from here."
+    body: "Bananas are great. They are the best in the world. Oh how i do wish i was a banana. Oh banana oh banana, please take me away from here.", 
+    topic_id: Topic.last.id
     )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
      title: "Portfolio Title: #{portfolio_item}",
-     subtitle: "My great service",
+     subtitle: "Ruby on rails",
+     body: "Bananas are great for me, they help keep me alive and healthy. bananas help me to not be hungry. please, oh please, give me a basket full of bananas.",
+     main_image: "http:placehold.it/600x400",
+     thumb_image: "http:placehold.it/350x200"
+    )
+end
+  
+  1.times do |portfolio_item|
+  Portfolio.create!(
+     title: "Portfolio Title: #{portfolio_item}",
+     subtitle: "angular",
      body: "Bananas are great for me, they help keep me alive and healthy. bananas help me to not be hungry. please, oh please, give me a basket full of bananas.",
      main_image: "http:placehold.it/600x400",
      thumb_image: "http:placehold.it/350x200"
